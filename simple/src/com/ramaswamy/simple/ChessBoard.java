c951b7d2d8a8aeaecff874f5a4ab1de8df8894dd
ramaswamy11703 <ramaswamy@gmail.com>
2016-02-21 19:12:47 -0800

    start

---
+++ simple/src/com/ramaswamy/simple/ChessBoard.java
@@ -0,0 +1,10 @@
+package com.ramaswamy.simple;
+
+public class ChessBoard {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+
+}
---
+++ trial project/src/com/ramaswamy/ChessBoard2.java
@@ -0,0 +1,13 @@
+package com.ramaswamy;
+
+public class ChessBoard2 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+	
+	
+
+
+}
